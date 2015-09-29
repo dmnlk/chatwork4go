798e3a41cb9c91d86c39142fd31e41fb40aa03d6
dmnlk <seikima2demon@gmail.com>
2015-09-30 01:52:52 +0900

    log

--- chatwork4.go
+++ chatwork4.go
@@ -25,14 +25,15 @@ func NewClient(key string) *Client {
 }
 
 func (client *Client) GetMyStatus() {
+	fmt.Print("api call start")
 	var buf io.ReadWriter
 	buf = new(bytes.Buffer)
-	fmt.Print("a")
-	req, err := http.NewRequest("GET", "", buf)
+	req, err := http.NewRequest("GET", END_POINT_URL+"/my/status ", buf)
 	if err != nil {
-
+		fmt.Errorf("occur error")
 	}
 	req.Header.Add("X-ChatWorkToken", string(client.apikey))
 	hclient := &http.Client{Timeout: time.Duration(10 * time.Second)}
 	hclient.Do(req)
+	fmt.Print("api call end")
 }
