aa3f11e749fc65e138ef3a024befe961ed97db62
dmnlk <seikima2demon@gmail.com>
2015-10-13 00:48:06 +0900

    add tasks struct

--- Struct.go
+++ Struct.go
@@ -8,3 +8,21 @@ type Status struct {
 	UnreadNum      int `json:"unread_num"`
 	UnreadRoomNum  int `json:"unread_room_num"`
 }
+
+type Task struct {
+	AssignedByAccount struct {
+		AccountID      int    `json:"account_id"`
+		AvatarImageURL string `json:"avatar_image_url"`
+		Name           string `json:"name"`
+	} `json:"assigned_by_account"`
+	Body      string `json:"body"`
+	LimitTime int    `json:"limit_time"`
+	MessageID int    `json:"message_id"`
+	Room      struct {
+		IconPath string `json:"icon_path"`
+		Name     string `json:"name"`
+		RoomID   int    `json:"room_id"`
+	} `json:"room"`
+	Status string `json:"status"`
+	TaskID int    `json:"task_id"`
+}
