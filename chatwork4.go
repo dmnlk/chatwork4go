604e98b13a958f8531d80e5e80ca04213110ef98
dmnlk <seikima2demon@gmail.com>
2015-10-16 02:22:15 +0900

    return val

--- chatwork4.go
+++ chatwork4.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"time"
 	"github.com/k0kubun/pp"
+	"golang.org/x/tools/dashboard/retrybuilds"
 )
 
 const (
@@ -63,37 +64,40 @@ func (client *Client) GetMyStatus() (*Status, error) {
 	return res, nil
 }
 
-func (client *Client) GetMyTasks() {
+func (client *Client) GetMyTasks() (*Task, error) {
 	var buf io.ReadWriter
 	buf = new(bytes.Buffer)
 	req, err := http.NewRequest("GET", END_POINT_URL+"/my/tasks", buf)
 	if err != nil {
 		fmt.Errorf("occur error")
+		return nil, err
 	}
 	req.Header.Add("X-ChatWorkToken", string(client.apikey))
 	hclient := &http.Client{Timeout: time.Duration(10 * time.Second)}
 	resp, err := hclient.Do(req)
 	if err != nil {
 		fmt.Errorf("err")
-
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
 		fmt.Errorf(resp.Status)
-
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	val, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Errorf("err")
+		return nil, err
 	}
 
 	var res *Task
 	err = json.Unmarshal(val, &res)
 	if err != nil {
 		fmt.Errorf("error")
+		return nil, err
 	}
 
-	pp.Println(res.AssignedByAccount)
 
+	return res, nil
 }
