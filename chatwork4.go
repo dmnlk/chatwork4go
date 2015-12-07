3be2cf57dd2e0e559ca61a161a8b12cace8f483e
dmnlk <seikima2demon@gmail.com>
2015-12-08 00:40:18 +0900

    add body parameter

--- chatwork4.go
+++ chatwork4.go
@@ -110,7 +110,7 @@ func (client *Client) GetMyTasks() (*Task, error) {
 }
 
 func (client *Client) PostMesseages(roomId int, message string) error {
-	data := url.Values{"foo": {"bar"}}
+	data := url.Values{"body": {message}}
 	req, err := http.NewRequest("POST", END_POINT_URL+string(roomId)+"/message", strings.NewReader(data.Encode()))
 	if err != nil {
 		fmt.Errorf("occur error")
