ebf6733a2d18e9fff2e63bba89fe3821489786ee
dmnlk <seikima2demon@gmail.com>
2015-12-09 00:40:36 +0900

    sample

--- example/main.go
+++ example/main.go
@@ -12,4 +12,9 @@ func main() {
 	client, _ := chatwork4go.NewClient(apikey)
 	pp.Println(client.GetMyStatus())
 	pp.Println(client.GetMyTasks())
+	// post sample
+	err := client.PostMesseages(41380405, "aaa")
+	if err != nil {
+		pp.Println(err)
+	}
 }
